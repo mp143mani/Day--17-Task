c264e1df54e4515932484d93c014c05689070aa9
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-07-12 10:51:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-
-      function countdown(callback) {
-        var count = 10;
-        var interval = setInterval(function() {
-          count--;
-          document.getElementById("countdown").innerHTML = count;
-          if (count === 0) {
-            clearInterval(interval);
-            callback();
-          }
-        }, 1000);
-      }
-
-      countdown(function() {
-        document.getElementById("countdown").innerHTML = "Happy Independence Day!!";
+function countdown(callback) {
+  var count = 10;
+  var interval = setInterval(function() {
+    count--;
+    document.getElementById("countdown").innerHTML = count;
+    if (count === 0) {
+      clearInterval(interval);
+      callback(function() {
+        console.log("Callback inside the callback");
       });
- 
+    }
+  }, 1000);
+}
+
+countdown(function(callback1) {
+  document.getElementById("countdown").innerHTML = "Happy Independence Day!!";
+  callback1(function(callback2) {
+    console.log("Second callback");
+    callback2();
+  });
+});
